915e66e23d8cd783420c87c04a16c57ccd2a0d32
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2017-05-30 23:10:04 -0300

    game: add initial logic

--- src/gallows_game/core.clj
+++ src/gallows_game/core.clj
@@ -1,6 +1,31 @@
 (ns gallows-game.core
   (:gen-class))
 
+(def total-lifes 6)
+
+(defn lose []
+  (print "You lose!"))
+
+(defn win []
+  (print "You win!"))
+
+(defn missing-letters [word hits]
+  (remove (fn [letter] (contains? hits (str letter))) word))
+
+(defn hit-whole-word? [word hits]
+  (empty? (missing-letters word hits))
+)
+
+(defn game [lifes word hits]
+  (if (= lifes 0)
+    (lose)
+    (if (hit-whole-word? word hits)
+      (win)
+      (print "Try again!")
+    )
+  )
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
